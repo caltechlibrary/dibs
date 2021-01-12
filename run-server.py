fdcb67c278e13e5ace5d86e8f6d46f61098593cf
Michael Hucka <mhucka@caltech.edu>
2021-01-12 14:49:23 -0800

    Remove no-longer-needed run-server.py

--- run-server.py
+++
@@ -1,9 +0,0 @@
-from   peewee import *
-import bottle
-from   bottle import route, run, template
-from   bottle import get, post, request
-
-from   dibs.server import Server
-
-server = Server(host = 'localhost', port = 8080)
-server.run()
