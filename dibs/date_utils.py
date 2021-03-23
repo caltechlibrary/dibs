e45756f3c90ee8ee4e93641686b6689ceabaa446
Michael Hucka <mhucka@caltech.edu>
2021-03-23 12:53:34 -0700

    Add optional argument to human_datetime() to make it more flexible

--- dibs/date_utils.py
+++ dibs/date_utils.py
@@ -15,10 +15,10 @@ import arrow
 # Exported functions.
 # .............................................................................
 
-def human_datetime(value):
+def human_datetime(value, format = "%I:%M %p (%Z) on %Y-%m-%d"):
     '''Return a human-friendly string for the given datetime in local time.'''
     if not value:
         return None
-    time = arrow.get(value).to('local').strftime("%I:%M %p (%Z) on %Y-%m-%d")
+    time = arrow.get(value).to('local').strftime(format)
     # Stftime has no option to *not* zero-pad the numbers, so we have to do it:
     return time.lstrip('0')
