5983d0c7e979df6003e51e889b5c6920520d95c4
R. S. Doiel <rsdoiel@gmail.com>
2021-04-16 15:55:00 -0700

    Update update_person_schema.sql
    fixed some typos

--- update_person_schema.sql
+++ update_person_schema.sql
@@ -1,6 +1,6 @@
 BEGIN TRANSACTION;
-CREATE TABLE IF NOT EXISTS "person_new" (("id" INTEGER NOT NULL PRIMARY
-KEY, "uname" VARCHAR(255) NOT NULL, , "role" VARCHAR(255) NOT NULL, "display_name" VARCHAR(255) NOT NULL, "updated" INTEGER NOT NULL);
+CREATE TABLE IF NOT EXISTS "person_new" ("id" INTEGER NOT NULL PRIMARY
+KEY, "uname" VARCHAR(255) NOT NULL, "role" VARCHAR(255) NOT NULL, "display_name" VARCHAR(255) NOT NULL, "updated" INTEGER NOT NULL);
 INSERT INTO person_new SELECT id, uname, role, display_name, updated FROM person;
 DROP TABLE person;
 ALTER TABLE person_new RENAME TO person;
