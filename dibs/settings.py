9b10e341b8e022766ac9763bab1208aae1d17620
mhucka <mhucka@caltech.edu>
2021-09-02 16:20:11 -0700

    Previous code was totally broken -- new version works

--- dibs/settings.py
+++ dibs/settings.py
@@ -11,11 +11,16 @@ file "LICENSE" for more information.
 
 from   collections import OrderedDict
 from   configparser import ConfigParser
-from   decouple import config, AutoConfig
-from   decouple import RepositoryIni, RepositoryEnv, DEFAULT_ENCODING
+from   decouple import Config, AutoConfig, DEFAULT_ENCODING, Undefined, undefined
+from   decouple import RepositoryIni, RepositoryEnv, RepositoryEmpty
 import os
 from   sidetrack import log
 
+
+# Replacements for decouple classes.
+# .............................................................................
+# This is the minimum set of classes that has to be redefined in order to be
+# able to add a 'section' keyword argument to the initializers.
 
 class DIBSRepositoryIni(RepositoryIni):
     '''Subclass of decouple's RepositoryIni that accepts a section name.'''
@@ -30,14 +35,58 @@ class DIBSRepositoryIni(RepositoryIni):
 
 
     def __contains__(self, key):
-        return (key in os.environ or self.parser.has_option(self.section, key))
+        return (key in os.environ
+                or key in self.parser.sections()
+                or self.parser.has_option(self.section, key))
 
 
     def __getitem__(self, key):
-        return self.parser.get(self.section, key)
+        if key in self.parser.sections():
+            return self.parser[key]
+        else:
+            return self.parser.get(self.section, key)
 
 
-class DIBSConfig(AutoConfig):
+class DIBSConfig(Config):
+    def __init__(self, repository, section = 'dibs'):
+        self.repository = repository
+        self.section = section
+
+
+    def get(self, option, default = undefined, cast = undefined, section = undefined):
+        '''Return the value for option or default if defined.'''
+        # We can't avoid __contains__ because value may be empty.
+        if isinstance(section, Undefined):
+            section = self.section
+        if option in os.environ:
+            value = os.environ[option]
+        elif section != undefined and section + '.' + option in os.environ:
+            value = os.environ[section + '.' + option]
+        elif option in self.repository:
+            value = self.repository[option]
+        elif section != undefined and section in self.repository.parser.sections():
+            value = self.repository[section][option]
+        else:
+            if isinstance(default, Undefined):
+                raise UndefinedValueError(f'Setting variable {option} not found.')
+            value = default
+
+        if isinstance(cast, Undefined):
+            cast = self._cast_do_nothing
+        elif cast is bool:
+            cast = self._cast_boolean
+
+        return cast(value)
+
+
+    def __call__(self, *args, **kwargs):
+        """
+        Convenient shortcut to get.
+        """
+        return self.get(*args, **kwargs)
+
+
+class DIBSAutoConfig(AutoConfig):
     '''Subclass of decouple's AutoConfig that uses DIBSRepositoryIni.'''
 
     SUPPORTED = OrderedDict([
@@ -46,9 +95,19 @@ class DIBSConfig(AutoConfig):
     ])
 
 
-    def __init__(self, search_path = None):
-        log(f'initializing DIBSConfig with search_path = {search_path}')
-        super().__init__(search_path)
+    def _load(self, path):
+        # Avoid unintended permission errors
+        try:
+            filename = self._find_file(os.path.abspath(path))
+        except Exception:
+            filename = ''
+        Repository = self.SUPPORTED.get(os.path.basename(filename), RepositoryEmpty)
+        self.config = DIBSConfig(Repository(filename, encoding = self.encoding))
 
+
+# Main exported symbol
+# .............................................................................
+# This is what users of this modules actually use.  In decouple, this is set
+# to decouple's AutoConfig.
 
-config = DIBSConfig()
+config = DIBSAutoConfig()
