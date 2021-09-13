750c0a22018ec5cc75b38bd70459c0785cff2336
mhucka <mhucka@caltech.edu>
2021-09-13 14:52:20 -0700

    Start a file for image utilities

---
+++ dibs/image_utils.py
@@ -0,0 +1,27 @@
+'''
+image_utils.py: miscellaneous image-handling utilities for DIBS
+
+Copyright
+---------
+
+Copyright (c) 2021 by the California Institute of Technology.  This code
+is open-source software released under a 3-clause BSD license.  Please see the
+file "LICENSE" for more information.
+'''
+
+from io import BytesIO
+from PIL import Image
+from sidetrack import log
+
+
+# Exported functions
+# .............................................................................
+
+def as_jpeg(byte_array):
+    '''Convert an image, stored as an array of bytes, into JPEG format.'''
+    img = Image.open(BytesIO(byte_array))
+    img = img.convert('RGB')
+    buffer = BytesIO()
+    img.save(buffer, 'JPEG')
+    img.close()
+    return buffer.getvalue()
