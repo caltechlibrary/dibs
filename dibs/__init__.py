40c3bbb5bad3f4fe050fdce28a3ca836f20877c1
mhucka <mhucka@caltech.edu>
2021-09-17 12:28:00 -0700

    Update version number

--- dibs/__init__.py
+++ dibs/__init__.py
@@ -22,8 +22,8 @@ file "LICENSE" for more information.
 #  |    by the Makefile. Manual changes to these values will be lost.      |
 #  ╰────────────────────── Notice ── Notice ── Notice ─────────────────────╯
 
-__version__     = '0.4.1'
-__description__ = 'An implementation of Controlled Digital Lending'
+__version__     = '0.5.0'
+__description__ = 'DIBS (Digital Borrowing System) is an implementation of Controlled Digital Lending'
 __url__         = 'https://github.com/caltechlibrary/dibs'
 __author__      = 'Michael Hucka, Robert S. Doiel, Tommy Keswick, Stephen Davison'
 __email__       = 'helpdesk@library.caltech.edu'
