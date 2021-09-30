7e6edc9942ac4267536612905fa46351d09e3b2c
mhucka <mhucka@caltech.edu>
2021-09-29 18:22:38 -0700

    Ignore more README.md files in the docs tree

--- docs/conf.py
+++ docs/conf.py
@@ -40,7 +40,8 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'README.md']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'README.md',
+                    '**/README.md']
 
 
 # -- Sphinx options for HTML output -------------------------------------------
