fd07f71f01cd15084f44df7295915959d5ac0dbf
mhucka <mhucka@caltech.edu>
2022-04-27 20:29:26 -0700

    Minor cleanup

--- tests/test_resolved_path.py
+++ tests/test_resolved_path.py
@@ -1,22 +1,21 @@
 import os
-from   os.path import dirname, join, isabs, isdir, exists, abspath, realpath
-import pytest
+from   os.path import join, dirname, abspath
 import sys
-import datetime as dt
-
 
 try:
-    thisdir = os.path.dirname(os.path.abspath(__file__))
-    sys.path.append(os.path.join(thisdir, '..'))
-except:
+    thisdir = dirname(abspath(__file__))
+    sys.path.append(join(thisdir, '..'))
+except Exception:
     sys.path.append('..')
 
-from dibs.settings import config, resolved_path
+from dibs.settings import resolved_path
+
 
 def test_relative_to_caller():
     me = __file__
     d = resolved_path('tests/test_resolved_path.py')
     assert d == abspath(me)
+
 
 def test_relative_to_settings():
     here = dirname(__file__)
