f0fcf6c954553df5aeaa6d8d4c4f8cd0f3d7b76b
mhucka <mhucka@caltech.edu>
2022-04-29 13:05:59 -0700

    Add tests for human_datetime()

--- tests/test_date_utils.py
+++ tests/test_date_utils.py
@@ -10,7 +10,7 @@ try:
 except:
     sys.path.append('..')
 
-from dibs.date_utils import *
+from dibs.date_utils import round_minutes, human_datetime
 
 
 def test_round_minutes():
@@ -33,3 +33,22 @@ def test_round_minutes():
     x = datetime(2021, 4, 6, 23, 0, 0, 0)
     assert round_minutes(x, 'down') == datetime(2021, 4, 6, 23, 0)
     assert round_minutes(x, 'up')   == datetime(2021, 4, 6, 23, 1)
+
+
+def test_human_datetime():
+    x = datetime(2021, 4, 6, 23, 0, 0, 0)
+    hx = human_datetime(x)
+    assert hx[0:8] == '4:00 PM '
+    assert hx[13:] == ' on 2021-04-06'
+
+    x = datetime(2021, 4, 6, 23, 5, 6, 7)
+    hx = human_datetime(x)
+    assert hx[0:8] == '4:05 PM '
+    assert hx[13:] == ' on 2021-04-06'
+
+    x = datetime(2021, 4, 6, 23, 5, 6, 7)
+    hx = human_datetime(x, "%I:%M %p on %Y-%m-%d")
+    assert hx == '4:05 PM on 2021-04-06'
+
+    hx = human_datetime('')
+    assert hx == ''
