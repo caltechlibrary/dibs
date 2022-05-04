84530f16f45b881398bdb91dce54816cabff4077
mhucka <mhucka@users.noreply.github.com>
2022-05-04 00:55:18 +0000

    deploy: c83811f800004260231e5d9ccc6108d3a9a0a5f7

--- _downloads/68116c2b27c8bf02b569723fcc075aa0/lsp.py
+++ _downloads/68116c2b27c8bf02b569723fcc075aa0/lsp.py
@@ -4,7 +4,7 @@ lsp.py: DIBS interface to LSPs.
 Copyright
 ---------
 
-Copyright (c) 2021 by the California Institute of Technology.  This code
+Copyright (c) 2021-2022 by the California Institute of Technology.  This code
 is open-source software released under a 3-clause BSD license.  Please see the
 file "LICENSE" for more information.
 '''
