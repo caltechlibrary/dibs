38382b73efe305c9a8d3d0d0f07cc6ca481ac634
Michael Hucka <mhucka@caltech.edu>
2022-10-04 16:48:55 -0700

    Remove hacks to set sys.path

--- tests/test_resolved_path.py
+++ tests/test_resolved_path.py
@@ -1,23 +1,18 @@
 import os
-from   os.path import join, dirname, abspath
-import sys
-
-try:
-    thisdir = dirname(abspath(__file__))
-    sys.path.append(join(thisdir, '..'))
-except Exception:
-    sys.path.append('..')
-
-from dibs.settings import resolved_path
+from os.path import join, dirname, abspath
 
 
 def test_relative_to_caller():
+    from dibs.settings import resolved_path
+
     me = __file__
     d = resolved_path('tests/test_resolved_path.py')
     assert d == abspath(me)
 
 
 def test_relative_to_settings():
+    from dibs.settings import resolved_path
+
     here = dirname(__file__)
     other = abspath(join(here, os.pardir, 'admin/run-server'))
     d = resolved_path('admin/run-server')
